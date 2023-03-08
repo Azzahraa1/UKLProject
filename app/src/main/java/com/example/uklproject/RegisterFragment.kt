2e44dfba2f299fef46100974d46f0823a445ce9e
Azzahra1 <fatimahazzahra4121@gmail.com>
2023-03-08 08:10:39 +0700

    second commit

---
+++ app/src/main/java/com/example/uklproject/RegisterFragment.kt
@@ -0,0 +1,11 @@
+package com.example.uklproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_fragment)
+    }
+}
